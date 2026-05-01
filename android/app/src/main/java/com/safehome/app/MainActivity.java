7698e0f80101118898745f7ab6227619255ac502
Rahul sah <sahxrahul@gmail.com>
2026-05-01 16:47:06 +0545

    removed android

--- android/app/src/main/java/com/safehome/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.safehome.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.safehome.app',
-  appName: 'Safe Home',
-  webDir: 'out'
-};
-
-export default config;
